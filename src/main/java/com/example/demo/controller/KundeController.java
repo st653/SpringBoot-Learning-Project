56fa857d07e52128e59e79eeec8e34f10c3c0dc4
st653 <118625971+st653@users.noreply.github.com>
2025-01-06 12:56:46 +0100

    update controller, repository

--- src/main/java/com/example/demo/controller/KundeController.java
+++ src/main/java/com/example/demo/controller/KundeController.java
@@ -17,21 +17,25 @@ public class KundeController {
 
     @PostMapping
     public Kunde neuerKunde(@RequestParam String name) {
+        System.out.println("New Post Request with name: " + name);
         return kundeService.erstelleKunde(name);
     }
 
     @GetMapping
     public List<Kunde> alleKunden() {
+        System.out.println("New Get Request for all customers");
         return kundeService.alleKundenAbrufen();
     }
 
     @GetMapping("/{id}")
     public Kunde kundeNachId(@PathVariable Long id) {
+        System.out.println("New Get Request for customer with id: " + id);
         return kundeService.findeKundeNachId(id);
     }
 
     @DeleteMapping("/{id}")
     public String loescheKunde(@PathVariable Long id) {
+        System.out.println("New Delete Request for customer with id: " + id);
         kundeService.loescheKunde(id);
         return "Kunde mit ID " + id + " wurde gelöscht.";
     }
--- src/main/java/com/example/demo/repository/KundeRepository.java
+++ src/main/java/com/example/demo/repository/KundeRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Kunde;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface KundeRepository extends JpaRepository<Kunde, Long> {
 }
 
