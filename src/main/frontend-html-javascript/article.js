f3c3dcbbad37e4da5459f6cd28db23d59eb00bc6
st653 <118625971+st653@users.noreply.github.com>
2025-03-21 13:03:24 +0100

    update Frontend article page

--- src/main/frontend-html-javascript/article.js
+++ src/main/frontend-html-javascript/article.js
@@ -1,5 +1,20 @@
 document.getElementById("articleButton").addEventListener("click", loadArticles);
 
+// Warte bis das HTML-Dokument vollständig geladen wurde
+document.addEventListener("DOMContentLoaded", function () {
+    loadHeader();
+});
+
+//Lade den Header
+function loadHeader() {
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector("header").innerHTML = data;
+        })
+        .catch(error => console.error("Fehler beim Laden des Headers:", error));
+}
+
 function loadArticles() {
     fetch("http://localhost:8080/artikel")
         .then(response => response.json())
